71cc0ea709068c30a441ad62a5cec096b7357be5
Francisco Dimmer <ftdimmer@gmail.com>
2022-09-01 21:53:38 -0300

    added all the remaining products

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import '../Styles/ProductCard.css'
 
-function ProductCard({ nombre, caudal, entrada, salida, voltaje, consumo  }) {
+function ProductCard({ electrobomba, minisurtidor, nombre, caudal, entrada, salida, voltaje, consumo }) {
   return (
     <div className='product-card'>
       <h3 className='product-title'>{nombre}</h3>
       <img className='product-image' src={require(`../Images/Products/${nombre}.png`)} />
+      
+      {/* <p className='product-flow'>Caudal: {caudal} LTS/HORA</p>
+      <p className='product-entrada'>Entrada: {entrada}"</p>
+      <p className='product-salida'>Salida: {salida}"</p>
+      <p className='product-salida'>Voltaje: {voltaje}</p>
+      <p className='product-salida'>Consumo: {consumo}</p> */}
+
+      {electrobomba ? (<div className='electrobomba-div'>
       <p className='product-flow'>Caudal: {caudal} LTS/HORA</p>
       <p className='product-entrada'>Entrada: {entrada}"</p>
       <p className='product-salida'>Salida: {salida}"</p>
       <p className='product-salida'>Voltaje: {voltaje}</p>
       <p className='product-salida'>Consumo: {consumo}</p>
-      
+      </div>) : ('')}
+
+      {minisurtidor ? (<div className='electrobomba-div'>
+      <p className='product-flow'>Caudal: {caudal} LTS/HORA</p>
+      <p className='product-entrada'>Entrada: {entrada}"</p>
+      <p className='product-salida'>Salida: {salida}"</p>
+      <p className='product-salida'>Voltaje: {voltaje}</p>
+      <p className='product-salida'>Consumo: {consumo}</p>
+      </div>) : ('')}
+
     </div>
   )
 }
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -7,9 +7,10 @@ function Products() {
 		<div className='products-container'>
 			<h1>NUESTROS PRODUCTOS</h1>
 				<h2>ELECTROBOMBAS</h2>
-				<p className='important-message'>IMPORTANTE: EL RENDIMIENTO FINAL DE LA ELECTROBOMBA ESTA DEFINIDO POR LOS ACCESORIOS QUE SE LE COLOCAN A LA MISMA</p>
+				<p className='important-message'>IMPORTANTE: EL RENDIMIENTO FINAL DE LA ELECTROBOMBA ESTA DEFINIDO POR LOS ACCESORIOS QUE SE LE COLOCAN A LA MISMA.</p>
 				<div className='electrobombas'>
-					<ProductCard 
+					<ProductCard
+					electrobomba={true} 
 					nombre='S4000'
 					caudal='4000'
 					entrada=''
@@ -17,7 +18,8 @@ function Products() {
 					voltaje='12V'
 					consumo=''
 					/>
-					<ProductCard 
+					<ProductCard
+					electrobomba={true} 
 					nombre='S7000'
 					caudal='7000'
 					entrada=''
@@ -25,7 +27,8 @@ function Products() {
 					voltaje='12V o 24V'
 					consumo=''
 					/>
-					<ProductCard 
+					<ProductCard
+					electrobomba={true} 
 					nombre='S15000'
 					caudal='15000'
 					entrada=''
@@ -33,7 +36,8 @@ function Products() {
 					voltaje='12V'
 					consumo=''
 					/>
-					<ProductCard 
+					<ProductCard
+					electrobomba={true} 
 					nombre='S21000'
 					caudal='21000'
 					entrada=''
@@ -43,8 +47,10 @@ function Products() {
 					/>
 				</div>
 				<h2>MINISURTIDORES</h2>
+				<p className='important-message'>IMPORTANTE: LOS MINISURTIDORES INCLUYEN 4 MTS. DE MANGUERA Y PICO A ELECCIÓN (TAMBIÉN SE PUEDE PEDIR SIN ESTOS ACCESORIOS).</p>
 				<div className='minisurtidores'>
-				<ProductCard 
+				<ProductCard
+					minisurtidor={true}
 					nombre='Minisurtidor S7000'
 					caudal='0000'
 					entrada='0'
@@ -52,7 +58,8 @@ function Products() {
 					voltaje='12V'
 					consumo='0'
 					/>
-					<ProductCard 
+					<ProductCard
+					minisurtidor={true} 
 					nombre='Minisurtidor S15000'
 					caudal='0000'
 					entrada='0'
@@ -60,7 +67,8 @@ function Products() {
 					voltaje='12V'
 					consumo='0'
 					/>
-					<ProductCard 
+					<ProductCard
+					minisurtidor={true} 
 					nombre='Minisurtidor S15000'
 					caudal='0000'
 					entrada='0'
@@ -72,7 +80,55 @@ function Products() {
 				<h2>ACCESORIOS</h2>
 				<div className='accesorios'>
 				<ProductCard 
-					nombre='Minisurtidor S15000'
+					nombre='Caudalímetro Estándar'
+					caudal='0000'
+					entrada='0'
+					salida='0'
+					voltaje='12V'
+					consumo='0'
+					/>
+				<ProductCard 
+					nombre='Caudalímetro Grande'
+					caudal='0000'
+					entrada='0'
+					salida='0'
+					voltaje='12V'
+					consumo='0'
+					/>
+				<ProductCard 
+					nombre='Pico Manual'
+					caudal='0000'
+					entrada='0'
+					salida='0'
+					voltaje='12V'
+					consumo='0'
+					/>
+				<ProductCard 
+					nombre='Pico Automático Estándar'
+					caudal='0000'
+					entrada='0'
+					salida='0'
+					voltaje='12V'
+					consumo='0'
+					/>
+				<ProductCard 
+					nombre='Pico Automático Grande'
+					caudal='0000'
+					entrada='0'
+					salida='0'
+					voltaje='12V'
+					consumo='0'
+					/>
+				<ProductCard 
+					nombre='Manguera'
+					caudal='0000'
+					entrada='0'
+					salida='0'
+					voltaje='12V'
+					consumo='0'
+					/>
+				<ProductCard 
+					nombre='Filtro'
 					caudal='0000'
 					entrada='0'
 					salida='0'
